3d697d353b33ddd0b58af2cdcc0227d7218a2e9f
freddyjpb <freddyjpb@gmail.com>
2023-02-28 09:22:50 -0600

    add category to buttons

--- components/atom/CategoryButton.tsx
+++ components/atom/CategoryButton.tsx
@@ -6,7 +6,7 @@ export default function CategoryButton({ children, ...props }: Props) {
   return (
     <button
       {...props}
-      className="relative w-[240px] h-[45px] box-border flex flex-row items-center justify-center px-[15px] py-[19px] rounded-3xl border border-gray  gap-[10px] font-sm text-gray bg-white font-medium leading-4 hover:scale-105 ease-in duration-150"
+      className="relative w-[auto] h-[45px] box-border flex flex-row items-center justify-center px-[10px] py-[19px] rounded-3xl border border-gray  gap-[10px] font-sm text-gray bg-white font-medium leading-4"
     >
       {children}
     </button>
