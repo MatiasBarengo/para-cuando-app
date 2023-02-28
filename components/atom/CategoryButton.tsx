f8e20513306b53b007e3cd87d9fb14ce4f46d251
freddyjpb <freddyjpb@gmail.com>
2023-02-28 08:47:05 -0600

    add category to buttons

--- components/atom/CategoryButton.tsx
+++ components/atom/CategoryButton.tsx
@@ -1,13 +1,3 @@
-/*export default function CategoryButton() {
-  return (
-    <button className="relative w-[240px] h-[45px] box-border flex flex-row items-center justify-center px-[15px] py-[19px] rounded-3xl border border-gray  gap-[10px] font-sm text-gray bg-white font-medium leading-4 hover:scale-105 ease-in duration-150">
-      Marcas y tiendas
-    </button>
-  );
-}*/
-
-import React from 'react';
-
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
